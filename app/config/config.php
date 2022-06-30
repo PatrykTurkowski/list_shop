4eedddd62be62a71a7ba2910af527d1427045fb3
patryk <patryk.turkowski21@gmail.com>
2022-06-30 23:27:08 +0200

    Add precompiled assets for Heroku

--- app/config/config.php
+++ app/config/config.php
@@ -12,6 +12,12 @@ define('APPROOT', dirname(dirname(__FILE__)));
 define('URLROOT', 'https://immense-island-56164.herokuapp.com/list_shop/');
 //path to css
 define('CSS_PATH', 'https://immense-island-56164.herokuapp.com/public/css/');
+<<<<<<< HEAD
 define('FONTELLO_PATH', 'https://immense-island-56164.herokuapp.com/icons/css/fontello.css');
 //path to JS
 define('JS_PATH', 'https://immense-island-56164.herokuapp.com/javascripts/');
+=======
+define('FONTELLO_PATH', 'https://immense-island-56164.herokuapp.com/public/icons/css/fontello.css');
+//path to JS
+define('JS_PATH', 'https://immense-island-56164.herokuapp.com/public/javascripts/');
+>>>>>>> 22b7354d474faf59b5ba29a32c0d42452b6df416
