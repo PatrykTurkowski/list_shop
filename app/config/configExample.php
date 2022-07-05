a92f03bc52934139ab9905e7429dfa3fcacc571d
patryk <patryk.turkowski21@gmail.com>
2022-07-05 13:06:39 +0200

    example commit

---
+++ app/config/configExample.php
@@ -0,0 +1,20 @@
+
+<?php
+//Database params
+
+//Database params
+define('DB_HOST', ''); //Add your db host
+define('DB_USER', ''); // Add your DB root
+define('DB_PASS', ''); //Add your DB pass
+define('DB_NAME', ''); //Add your DB Name
+
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT (Dynamic links)
+define('URLROOT', 'http://localhost/list_shop/');
+//path to css
+define('CSS_PATH', 'http://localhost/list_shop/public/css/');
+define('FONTELLO_PATH', 'http://localhost/list_shop/public/icons/css/fontello.css');
+//path to JS
+define('JS_PATH', 'http://localhost/list_shop/public/javascripts/');
