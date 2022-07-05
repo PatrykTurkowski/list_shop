0b106e08b7b590a301670e6bd50ae4bc13011a27
patryk <patryk.turkowski21@gmail.com>
2022-07-05 12:42:09 +0200

    final version

--- app/config/configExample.php
+++ app/config/configExample.php
@@ -3,18 +3,18 @@
 //Database params
 
 //Database params
-define('DB_HOST', ''); //Add your db host
-define('DB_USER', ''); // Add your DB root
-define('DB_PASS', ''); //Add your DB pass
-define('DB_NAME', ''); //Add your DB Name
+define('DB_HOST', 'eu-cdbr-west-03.cleardb.net'); //Add your db host
+define('DB_USER', 'b38c2c50355ee4'); // Add your DB root
+define('DB_PASS', '2b6488bf'); //Add your DB pass
+define('DB_NAME', 'heroku_21850fb451f0f46'); //Add your DB Name
 
 //APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URLROOT (Dynamic links)
-define('URLROOT', 'http://localhost/list_shop/');
+define('URLROOT', 'https://shoplistproduct.herokuapp.com/');
 //path to css
-define('CSS_PATH', 'http://localhost/list_shop/public/css/');
-define('FONTELLO_PATH', 'http://localhost/list_shop/public/icons/css/fontello.css');
+define('CSS_PATH', 'https://shoplistproduct.herokuapp.com/public/css/');
+define('FONTELLO_PATH', 'https://shoplistproduct.herokuapp.com/public/icons/css/fontello.css');
 //path to JS
-define('JS_PATH', 'http://localhost/list_shop/public/javascripts/');
+define('JS_PATH', 'https://shoplistproduct.herokuapp.com/public/javascripts/');
